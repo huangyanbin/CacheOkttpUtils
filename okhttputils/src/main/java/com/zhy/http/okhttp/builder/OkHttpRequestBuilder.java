97d5470e2f112afd9f2a9542656eac3e1debe097
David <873825232@qq.com>
2018-05-31 21:41:54 +0800

    Update OkHttpRequestBuilder.java
    提供获取params和url get和set方法

--- okhttputils/src/main/java/com/zhy/http/okhttp/builder/OkHttpRequestBuilder.java
+++ okhttputils/src/main/java/com/zhy/http/okhttp/builder/OkHttpRequestBuilder.java
@@ -52,4 +52,27 @@ public abstract class OkHttpRequestBuilder<T extends OkHttpRequestBuilder>
     }
 
     public abstract RequestCall build();
+ public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public Map<String, String> getParams() {
+        return params;
+    }
+
+    public void setParams(Map<String, String> params) {
+        this.params = params;
+    }
 }
